e093ca84774254846ab9eb29e766185d9f727196
Pauline Ricaud <pauline.ricaud.32@gmail.com>
2026-08-25 23:04:25 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
 
   ],
-  base: "/",
+  base: "https://paulinericaud.github.io/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
