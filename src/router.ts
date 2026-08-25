40ce3fd3488545f00edfd7233dee49046ac0282f
Pauline Ricaud <pauline.ricaud.32@gmail.com>
2026-08-25 17:59:05 +0200

    Update router.ts

--- src/router.ts
+++ src/router.ts
@@ -11,29 +11,29 @@ const router = createRouter({
     {
       path: '/en/profile',
       name: 'en-profile',
-      component: () => import("./components/en/Profile.vue"),
+      component: () => import("@/components/en/Profile.vue"),
     },
     {
       path: '/fr/profile',
       name: 'fr-profile',
-      component: () => import("./components/fr/Profile.vue"),
+      component: () => import("@/components/fr/Profile.vue"),
     },
     /*   PROJECTS   */
     {
       path: '/en/projects',
       name: 'en-projects',
-      component: () => import("./components/en/Projects.vue"),
+      component: () => import("@/components/en/Projects.vue"),
     },
     {
       path: '/fr/projects',
       name: 'fr-projects',
-      component: () => import("./components/fr/Projects.vue"),
+      component: () => import("@/components/fr/Projects.vue"),
     },
     /*    404    */
     {
       path: '/:pathMatch(.*)*',
       name: '404',
-      component: () => import("./components/404.vue"),
+      component: () => import("@/components/404.vue"),
     },
   ],
 })
